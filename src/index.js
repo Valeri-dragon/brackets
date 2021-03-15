7fdb1e236173fa1c42b99f10b2a243afa941e9bf
Valeri-dragon <yakontekst01@yandex.ru>
2021-03-15 20:40:59 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+   var arrayStr = [];
+   bracketsConfig.forEach(element => { arrayStr.push(element[0]+element[1]);
+       
+   });
+   while(str.length >0) {
+       var testingStr = str;
+       arrayStr.forEach(bracket => {
+           testingStr = testingStr.replace(bracket, '');
+       });
+       if (testingStr===str) {
+           return false;
+       }
+       str = testingStr;
+   }
+ return true;
 }
